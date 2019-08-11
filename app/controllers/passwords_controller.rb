6d1569378cd6b18d7d6345eaada45d463aec4681
Vincent <vincethewalker@yahoo.co.uk>
2019-08-11 19:25:32 +0100

    Created Controller Filters to ensure user is logged in

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,5 +1,5 @@
 class PasswordsController < ApplicationController
-  
+  before_action :authenticate_user!
   def index
     @passwords = Password.all
   end
--- app/models/password.rb
+++ app/models/password.rb
@@ -1,5 +1,4 @@
 class Password < ApplicationRecord
-    before_action :authenticate_user!
     validates :name, presence: true
     validates :user_name, presence: true
     belongs_to :user
