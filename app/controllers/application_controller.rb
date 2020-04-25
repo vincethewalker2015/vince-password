c4697a03821685db0337ecb440fde826fc850f32
vincentbwalker <vincethewalker@gmail.com>
2020-04-25 16:39:35 +0100

    Added User filters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,9 @@ class ApplicationController < ActionController::Base
   def authorize_admin
     redirect_to root_path, alert: 'Access Denied' unless current_user.admin?
   end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  
+  before_action :authenticate_user!
+  before_action :authorize_admin
   
   def index
     @users = User.all
