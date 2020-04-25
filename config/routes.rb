17f195cd6f2f68ae134e64833258c466acf142a4
vincentbwalker <vincethewalker@gmail.com>
2020-04-25 19:56:25 +0100

    Created Admin-User delete feature and links

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   
   devise_for :users, :controllers => { :registrations => 'registrations'}
-  resources :users, only: [:show, :index]
+  match 'users/:id' => 'users#destroy', :via => :delete, :as => :admin_destroy_user
+  resources :users
   resources :passwords
 end
