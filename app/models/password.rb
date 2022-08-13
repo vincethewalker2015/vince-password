b40d74e8885a6eb05038bbaa90784b11633f87e6
Vincent <vincethewalker@yahoo.co.uk>
2022-08-13 21:49:15 +0100

    Set up search in Model

--- app/models/password.rb
+++ app/models/password.rb
@@ -1,6 +1,21 @@
 class Password < ApplicationRecord
-    validates :name, presence: true
-    validates :user_name, presence: true
-    #belongs_to :user
+	validates :name, presence: true
+	validates :user_name, presence: true
+	#belongs_to :user
+
+	def self.matches(field_name, param) 
+		Password.where("#{field_name} like ?", "%#{param}%")
+	end
+
+	def self.search(param)
+		param.strip!
+		to_send_back = name_matches(param).uniq
+		return nil unless to_send_back
+		to_send_back
+	end
+
+	def self.name_matches(param) 
+		matches('cast(name as text)', param)
+	end
     
 end
