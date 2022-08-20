68d95a9a8ce9f26e338e59a2d85bbc17763ae5d5
Vincent <vincethewalker@yahoo.co.uk>
2022-08-20 19:18:00 +0100

    Added feature where text spaces for notes will show in show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def format_linebreaks(text)
+    safe_text = h(text)
+    paragraphs = split_paragraphs(safe_text).map(&:html_safe)
+  
+    html = ''.html_safe
+    paragraphs.each do |paragraph|
+      html << content_tag(:p, paragraph)
+    end
+    html
+  end
 end
